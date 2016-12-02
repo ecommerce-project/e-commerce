0d30bb7ef84cc07ca18240d749805e02d0e858ab
bryanati00 <bryanati00@gmail.com>
2016-12-02 00:51:29 -0500

    test

---
+++ src/gottaHaveIt/CartItem.java
@@ -0,0 +1,30 @@
+package gottaHaveIt;
+
+public class CartItem {
+	private Product p;
+	private int quantity;
+
+
+	public CartItem() {
+		super();
+	}
+	public CartItem(Product p, int quantity) {
+		super();
+		this.p = p;
+		this.quantity = quantity;
+	}
+	public Product getP() {
+		return p;
+	}
+	public void setP(Product p) {
+		this.p = p;
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+
+}
